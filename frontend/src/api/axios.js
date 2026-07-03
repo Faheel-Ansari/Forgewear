5be6adc8cf08d8ebd56ea5c804eada2f1247dbf3
Faheel Ansari <faheelansari8@gmail.com>
2026-07-03 16:01:18 +0500

    Initial commit: setup frontend and backend structure

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,16 @@
 import axios from "axios"
 
-export const BaseURL = "http://localhost:8000"
+export const BaseURL = import.meta.env.VITE_BASE_URL
 
 export const getCSRF = async () => {
-    await axios.get(`${BaseURL}/sanctum/csrf-cookie`,{
+    await axios.get(`${BaseURL}/sanctum/csrf-cookie`, {
         withCredentials: true
     })
 }
 
+const appURL = import.meta.env.VITE_APP_URL
 export default axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: appURL,
     withCredentials: true,
     withXSRFToken: true,
     xsrfCookieName: "XSRF-TOKEN",
