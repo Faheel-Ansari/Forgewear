5673001f5ac6ac38279b1cc506d374851d3421a2
Faheel Ansari <faheelansari8@gmail.com>
2026-07-11 19:54:58 +0500

    fixed small bugs.

--- frontend/src/components/reviews/review.js
+++ frontend/src/components/reviews/review.js
@@ -33,6 +33,7 @@ export function useReview() {
             dispatch(setLoading(false));
 
             const res = await api.get(`/review/detail/${id}`);
+            console.log(res);
             
             if (res.data.status === true) {
                 dispatch(setIsAvailable(true));
